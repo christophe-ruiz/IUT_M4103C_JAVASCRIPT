2804be3a160a3e6117eb71917701959c86a3e3b3
Christophe RUIZ <christophe.ruiz@etu.univ-amu.fr>
2020-04-12 18:46:17 +0200

    regex pour les caractères spéciaux modifiés (bricolage à mon avis)

--- json/signup.php
+++ json/signup.php
@@ -11,7 +11,7 @@ $obj -> success = false;
 $db = new Database();
 
 $usr = $_POST['username'];
-$pwd = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
+$pwd = $_POST['pwd'];
 $mail = $_POST['mail'];
 
 if (empty($usr) || empty($pwd) || empty($mail) ) {
@@ -73,6 +73,7 @@ if(!preg_match('/.{8,}/', $pwd)) {
 }
 
 if (!isset($obj -> pwdChecks) && !isset($obj -> usrChecks) && !isset($obj -> mailChecks)) {
+    $pwd = password_hash($pwd, PASSWORD_DEFAULT);
     try {
         $stmt = $db
             -> pdo()
